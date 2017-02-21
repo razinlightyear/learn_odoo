34cd91fab7f3d35647eea91c697e460d7a48942f
Andrew Emrazian <razinlightyear@gmail.com>
2017-02-21 09:23:16 -0700

    Added inheritance stuff.

--- my_module/models/library_book.py
+++ my_module/models/library_book.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
-from openerp import models, fields
-from openerp.addons import decimal_precision as dp 
+from openerp import models, fields, api
+from openerp.fields import Date as fDate
+from openerp.addons import decimal_precision as dp
+from datetime import timedelta as td
+
 class LibraryBook(models.Model):
 	_name = 'library.book'
+	_inherit = ['base.archive']
 	_description = 'Library Book'
 	_order = 'date_release desc, name'
 	_rec_name = 'short_name'
@@ -45,7 +49,7 @@ class LibraryBook(models.Model):
 		'Reader Average Rating', 
 		(1, 14), # Optiona; precision (total, decimals)
 	)
-	active = fields.Boolean('Active',default=True) # Ability to remove something from the library (I added)
+	#active = fields.Boolean('Active',default=True) # Ability to remove something from the library (I added). Inherited by abstract model
 	cost_price = fields.Float('Book Cost', dp.get_precision('Book Price'))
 	currency_id = fields.Many2one('res.currency', string='Currency') # Stores the currency to be used.
 	retail_price = fields.Monetary('Retail Price', currency_field='currency_id') # optional: currency_field='currency_id'
@@ -55,6 +59,16 @@ class LibraryBook(models.Model):
 		string='Publisher',
 		# optional: ondelete='set null', context={}, domain=[]
 	)
+	publisher_city = fields.Char('Publisher City', related='publisher_id.city')
+	age_days = fields.Float(
+		string='Days Since Release',
+		compute='_compute_age',
+		inverse='_inverse_age',
+		search='_search_age',
+		store=False,
+		compute_sudo=False,
+	)
+	ref_doc_id = fields.Reference(selection='_referencable_models',string='Reference Document')
 
 	def name_get(self):
 		result = []
@@ -64,7 +78,75 @@ class LibraryBook(models.Model):
 				)
 		return result
 
+
+	# Validations
+	_sql_constraints = [
+		('name_uniq',
+		 'UNIQUE (name)',
+		 'Book title must be unique.')
+	]
+
+	@api.constrains('date_release')
+	def _check_release_date(self):
+		for r in self:
+			if r.date_release > fields.Date.today():
+				raise models.ValidationError(
+					'Release date must be in the past')
+
+	# used for age_days (computed attribute)
+	@api.depends('date_release')
+	def _compute_age(self):
+		today = fDate.from_string(fDate.today())
+		for book in self.filtered('date_release'):
+			delta = (fDate.from_string(book.date_release - today))
+			book.age_days = delta.days
+
+	# used for age_days (computed attribute)
+	def _inverse_age(self): 
+		today = fDate.from_string(fDate.today())
+		for book in self.filered('date_release'):
+			d = td(days=book.age_days) - today
+			book.date_release = fDate.to_string(d)
+
+	# used for age_days (computed attribute)
+	def _search_age(self, operator, value):
+		today = fDate.from_string(fDate.today())
+		value_days = td(days=value)
+		value_date = fDate.to_string(today - value_days)
+		return [('date_release', operator, value_date)]	
+
+	@api.model
+	def _referencable_models(self):
+		models = self.env['res.request.link'].search([])
+		return [(x.object, x.name) for x in models]
+
+	class BaseArchive(models.AbstractModel):
+		_name = 'base.archive'
+		active = fields.Boolean(default=True)
+
+		def do_archive(self):
+			for record in self:
+				record.active = not record.active
+
 class ResPartner(models.Model):
 	_inherit = 'res.partner'
+	_order = 'name'
 	book_ids = fields.One2many('library.book','publisher_id',string='Published Books')
-	book_ids = fields.Many2many('library.book',string='Authored Books')
+	#book_ids = fields.Many2many('library.book',string='Authored Books')
+	authored_book_ids = fields.Many2many('library.book', string='Authored Books')
+	count_books = fields.Integer('Number of Authored Books', compute='_compute_count_books')
+
+	@api.depends('authored_book_ids')
+	def _compute_count_books(self):
+		for r in self:
+			r.count_books = len(r.authored_book_ids)
+
+class LibraryMember(models.Model):
+	_name = 'library.member'
+	_inherits = {'res.partner':'partner_id'}
+	partner_id = fields.Many2one('res.partner',ondelete='cascade')
+
+	date_start = fields.Date('Member Since')
+	date_end = fields.Date('Termination Date')
+	member_number = fields.Char()
+	
